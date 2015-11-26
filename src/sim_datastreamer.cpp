8a32cb7d6ead854f66fead3a8388669d6e6aef74
Marius Kaufmann <marius.kaufmann@googlemail.com>
2015-11-26 20:13:27 +0100

    revamped sim_datastreamer to use client clas

--- src/sim_datastreamer.cpp
+++ src/sim_datastreamer.cpp
@@ -2,21 +2,10 @@
 #include <iostream>
 #include "input/input_strategy.h"
 #include "input/dummy_input_strategy.h"
-
-const char* test(taylortrack::input::InputStrategy& test) {
-    return test.read();
-}
+#include "sim/streamer.h"
 
 int main(int argc, char *argv[]) {
     taylortrack::input::DummyInputStrategy dummy = taylortrack::input::DummyInputStrategy();
-    std::cout << test(dummy) << std::endl;
-
-    yarp::os::Network yarp;
-    yarp::os::BufferedPort<yarp::os::Bottle> out_port;
-    out_port.open("/bp39_out");
-    yarp.connect(out_port.getName(),"/bp39_in");
-    yarp::os::Bottle& output = out_port.prepare();
-    output.clear();
-    output.addString("test string");
-    out_port.write(true);
+    taylortrack::sim::Streamer streamer = taylortrack::sim::Streamer(&dummy, "/bp39_out");
+    streamer.start_streaming("/bp39_in");
 }
