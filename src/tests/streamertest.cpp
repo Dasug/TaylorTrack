16f253b4cf7f487b2f281ee9c1d261332ca2451e
Marius Kaufmann <marius.kaufmann@googlemail.com>
2015-12-10 17:06:42 +0100

    added streamer testcase

---
+++ src/tests/streamertest.cpp
@@ -0,0 +1,15 @@
+//
+// Created by dasug on 10.12.15.
+//
+
+#include "../input/dummy_input_strategy.h"
+#include "../sim/streamer.h"
+#include <gtest/gtest.h>
+
+
+TEST(StreamerTest, FailWhenDone) {
+    taylortrack::input::DummyInputStrategy dummy = taylortrack::input::DummyInputStrategy();
+    taylortrack::sim::Streamer streamer = taylortrack::sim::Streamer(&dummy, "/bp39_out");
+    dummy.read();
+    EXPECT_FALSE(streamer.start_streaming("/test_in"));
+}
