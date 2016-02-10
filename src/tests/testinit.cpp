8e96dcee05e795cb3461b4ad534bbe40cefc1225
Marius Kaufmann <marius.kaufmann@googlemail.com>
2016-02-10 14:45:24 +0100

    removed obsolete debug output

--- src/tests/testinit.cpp
+++ src/tests/testinit.cpp
@@ -4,9 +4,6 @@
 
 
 int main(int argc, char **argv) {
-    //taylortrack::utils::WaveParser parser = taylortrack::utils::WaveParser("../Testdata/Test.wav");
-    //std::string samples = parser.get_samples(999999999999);
-    //std::cout << samples << std::endl;
 	testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
