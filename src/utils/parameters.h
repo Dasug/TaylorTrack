a2ff6220c813cc0403df44092bc6c44bb7eb61e0
Christian <christian.hack2@googlemail.com>
2016-02-10 17:58:44 +0100

    Doxygen for parameters.h

--- src/utils/parameters.h
+++ src/utils/parameters.h
@@ -1,16 +1,45 @@
-//
-// Created by christian on 05.02.16.
-//
-
+/**
+* @file
+* @brief Header file for the structs for the parameters.
+* @version 1
+*/
 #ifndef TAYLORTRACK_PARAMETERS_H
 #define TAYLORTRACK_PARAMETERS_H
 namespace taylortrack {
     namespace utils {
+        /**
+         * @Struct
+         * @brief Contains the given start parameters.
+        */
         struct Parameters {
+            /**
+             * @var *file
+             * Path to the file.
+            */
             const char *file;
+
+            /**
+             * @var *outport
+             * Output port which should be used.
+            */
             const char *outport = "/bp39_out";
+
+            /**
+             * @var *inport
+             * Input port which should be used.
+            */
             const char *inport = "/bp39_in";
+
+            /**
+             * @var size
+             * Size of packages that should be streamed
+            */
             int size = 0;
+
+            /**
+             * @var valid
+             * Stores if the given parameters are valid.
+            */
             bool valid = false;
         };
     }
