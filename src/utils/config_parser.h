ed647d9e4c0d380c15d1f8602d05db09ba9ea13d
Christian <christian.hack2@googlemail.com>
2016-02-10 17:51:53 +0100

    Doxygen for config_parser

--- src/utils/config_parser.h
+++ src/utils/config_parser.h
@@ -1,4 +1,8 @@
-
+/**
+* @file
+* @brief Header file for the configuration file parser.
+* @version 1
+*/
 #ifndef TAYLORTRACK_CONFIG_PARSER_H
 #define TAYLORTRACK_CONFIG_PARSER_H
 
@@ -9,19 +13,32 @@
 
 namespace taylortrack {
   namespace utils {
+    /**
+     * @class ConfigParser
+     * @brief Parses a configuration and stores the configuration at its representative struct.
+    */
     class ConfigParser {
      private:
+
       std::ifstream file_;
-      std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems);
-      std::vector<std::string> split(const std::string &s, char delim);
       std::string trim(const std::string &s);
+      std::vector<std::string> split(const std::string &s, char delim);
       Options options;
       Audio audio;
       Video video;
       Combination combination;
 
      public:
+      /**
+       * @brief Configuration parser constructor
+       * Opens the supplied configuration file and parses it.
+       * @param file_name Path to the file to be parsed.
+      */
       ConfigParser(const char *file_name);
+
+      /**
+       * @brief Starts parsing the file.
+       */
       void parse_file();
     };
   }
