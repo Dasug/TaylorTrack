c6de42b48b06ce44d1b98a6141af114f264350b7
Marius Kaufmann <marius.kaufmann@googlemail.com>
2016-02-10 12:25:12 +0100

    fixed implicit type cast warning

--- src/utils/wave_parser.cpp
+++ src/utils/wave_parser.cpp
@@ -82,7 +82,7 @@ void taylortrack::utils::WaveParser::parse_file() {
 
 
 std::string taylortrack::utils::WaveParser::get_samples(unsigned int sample_num) {
-    int transfered_sample_size = (int) ((this->block_align * sample_num) <= this->data_size ? this->block_align * sample_num : this->data_size);
+    unsigned long transfered_sample_size = ((this->block_align * sample_num) <= this->data_size ? this->block_align * sample_num : this->data_size);
     char samples[transfered_sample_size];
     for (int i = 0; i < transfered_sample_size; ++i) {
         samples[i] = (char) this->file_->get();
