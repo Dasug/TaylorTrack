6219b94958ea18e85ef7e0f427d6be2b6885c555
Marius Kaufmann <marius.kaufmann@googlemail.com>
2016-02-11 15:23:45 +0100

    built general layout

--- src/vis/OutputVisualizer.cpp
+++ src/vis/OutputVisualizer.cpp
@@ -1,4 +1,5 @@
 #include <ncurses.h>
+#include <string.h>
 #include "OutputVisualizer.h"
 
 taylortrack::vis::OutputVisualizer::OutputVisualizer(taylortrack::utils::Options options) {
@@ -26,6 +27,12 @@ taylortrack::vis::OutputVisualizer::OutputVisualizer(taylortrack::utils::Options
     if(has_colors()) {
         start_color();
 
+        // Define color pair 1 as white on blue
+        init_pair(1, COLOR_WHITE, COLOR_BLUE);
+
+        // Define color pair 2 as blue on white
+        init_pair(2, COLOR_BLUE, COLOR_WHITE);
+
         int rows, cols;
 
         // retrieve window size
@@ -34,8 +41,15 @@ taylortrack::vis::OutputVisualizer::OutputVisualizer(taylortrack::utils::Options
         this->rows = rows;
         this->cols = cols;
 
-        this->top_window = newwin(2, cols, 0, 0);
-        this->main_window = newwin(rows - 2, cols, 2, 0);
+        refresh();
+
+        this->top_window = newwin(4, cols, 0, 0);
+        // Set color pair 1 for thetop window
+        wbkgd(this->top_window, COLOR_PAIR(1));
+
+        this->main_window = newwin(rows - 4, cols, 4, 0);
+
+        update_top_window();
     } else {
         failed = true;
     }
@@ -54,12 +68,15 @@ taylortrack::vis::OutputVisualizer::~OutputVisualizer() {
 void taylortrack::vis::OutputVisualizer::draw_frame() {
     this->handle_resize();
     this->handle_user_input();
+    werase(this->main_window);
 
     // TODO Actual visualisation
 
+    // Create Box around main window
+    box(this->main_window, 0, 0);
 
     // flush display buffer and write to screen
-    refresh();
+    wrefresh(this->main_window);
 }
 
 void taylortrack::vis::OutputVisualizer::handle_resize() {
@@ -69,11 +86,13 @@ void taylortrack::vis::OutputVisualizer::handle_resize() {
     getmaxyx(stdscr,rows,cols);
 
     if(rows != this->rows || cols != this->cols) {
-        wresize(this->top_window, 2, cols);
-        wresize(this->main_window, rows - 2, cols);
+        wresize(this->top_window, 4, cols);
+        wresize(this->main_window, rows - 4, cols);
+        if(cols != this->cols) {
+            this->update_top_window();
+        }
         this->rows = rows;
         this->cols = cols;
-        mvprintw(0, 0, "(%d|%d)", cols, rows); // Debug output
 
     }
 }
@@ -106,4 +125,33 @@ bool taylortrack::vis::OutputVisualizer::user_has_quit() const {
 
 bool taylortrack::vis::OutputVisualizer::has_failed() const {
     return failed;
+}
+
+void taylortrack::vis::OutputVisualizer::update_top_window() {
+    werase(this->top_window);
+    box(this->top_window, 0, 0);
+    wmove(this->top_window, 1, 1);
+
+    this->print_center(this->top_window, "TaylorTrack - Tracking Visualizer");
+    wrefresh(this->top_window);
+}
+
+/* Prints String in the center of the window's current line
+ * Assumes a necessary padding in the size of the current cursor position (for box lines etc.)
+ * Sets the cursor to the next same x position in the next line afterwards
+ * */
+void taylortrack::vis::OutputVisualizer::print_center(WINDOW *window, const char *string) {
+    int cy, cx;
+
+    getyx(window, cy, cx);
+
+    int cols = getmaxx(window);
+
+    int position = ((cols - 2*cx) - (int) strlen(string)) / 2;
+
+    wmove(window, cy, cx + position);
+
+    wprintw(window, string);
+
+    wmove(window, cy + 1, cx);
 }
