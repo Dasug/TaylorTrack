fd19b788fe71680d25b0e2261aded2e7b919ef1a
Marius Kaufmann <marius.kaufmann@googlemail.com>
2016-02-11 14:13:26 +0100

    implemented main initialization and display loop

--- src/visualizer.cpp
+++ src/visualizer.cpp
@@ -1,3 +1,36 @@
-int main(int argc, char **argv) {
+#include <ncurses.h>
+#include <iostream>
+#include "vis/OutputVisualizer.h"
+#ifdef WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif //WIN32
 
+int main(int argc, char **argv) {
+    taylortrack::utils::Options options = taylortrack::utils::Options();
+    taylortrack::vis::OutputVisualizer *out = new taylortrack::vis::OutputVisualizer(options);
+
+    if(out->term_supports_color()) {
+        while(!out->has_failed() && !out->user_has_quit()) {
+            out->draw_frame();
+
+            // Obviously Windows and Unix systems use different sleep functions...
+            int milliseconds = 900;
+            #ifdef WIN32
+                Sleep(milliseconds);
+            #else
+                usleep((__useconds_t) (milliseconds * 1000));
+            #endif // win32
+        }
+
+        // Leave ncurses terminal mode
+        delete(out);
+    } else {
+        delete(out);
+        std::cout << "Your terminal doesn't support colors, aborting..." << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
