6d942f88001a710bc01dedbb8fb866f638dfef78
christian <christian.hack2@googlemail.com>
2016-02-14 19:13:41 +0100

    adjusted code to the google c++ style guide

--- src/input/read_file_input_strategy.cpp
+++ src/input/read_file_input_strategy.cpp
@@ -6,40 +6,39 @@
 #include "read_file_input_strategy.h"
 
 namespace taylortrack {
-    namespace input {
-        ReadFileInputStrategy::ReadFileInputStrategy(const char* file_name) {
-            file_ = new std::ifstream (file_name, std::ios::in|std::ios::binary|std::ios::ate);
+  namespace input {
+    ReadFileInputStrategy::ReadFileInputStrategy(const char *file_name) {
+      file_ = new std::ifstream(file_name, std::ios::in | std::ios::binary | std::ios::ate);
 
-            if(file_ -> fail()) {
-              file_ -> close();
-            }
+      if (file_->fail())
+        file_->close();
 
-            // Determine the size of the file and initialise the char array for the data..
-            if(file_->is_open()) {
-                size_ = (long) file_->tellg();
-                file_->seekg(0, std::ios::beg);
-            }
-            done_ = false;
-        }
-
-        yarp::os::Bottle ReadFileInputStrategy::read(yarp::os::Bottle& bottle) {
-            if(file_->is_open() && (size_ >= 0) ) {
-                char *memblock = new char[size_];
-                file_->read(memblock,size_);
-                bottle.addString(yarp::os::ConstString(memblock, size_));
-                file_->close();
-            }
-            done_ = true;
-            return bottle;
-        }
-
-        bool ReadFileInputStrategy::is_done() {
-            return done_;
-        }
-
-        ReadFileInputStrategy::~ReadFileInputStrategy() {
-            delete file_;
-        }
+      // Determine the size of the file and initialise the char array for the data..
+      if (file_->is_open()) {
+        size_ = (long) file_->tellg();
+        file_->seekg(0, std::ios::beg);
+      }
+      done_ = false;
     }
-}
+
+    yarp::os::Bottle ReadFileInputStrategy::read(yarp::os::Bottle &bottle) {
+      if (file_->is_open() && (size_ >= 0)) {
+        char *memblock = new char[size_];
+        file_->read(memblock, size_);
+        bottle.addString(yarp::os::ConstString(memblock, size_));
+        file_->close();
+      }
+      done_ = true;
+      return bottle;
+    }
+
+    bool ReadFileInputStrategy::is_done() {
+      return done_;
+    }
+
+    ReadFileInputStrategy::~ReadFileInputStrategy() {
+      delete file_;
+    }
+  } //namespace input
+} // namespace taylortrack
 // LCOV_EXCL_BR_STOP
