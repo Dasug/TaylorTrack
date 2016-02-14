d32c9995a80a522cc98cdf0afd9a87a4c1c8ea21
christian <christian.hack2@googlemail.com>
2016-02-14 20:28:27 +0100

    adjusted code to the google c++ style guide,

--- src/utils/fft_lib.cpp
+++ src/utils/fft_lib.cpp
@@ -1,62 +1,49 @@
-//
-// Created by Jannis Hoppe on 28.01.16.
-//
-#include <iostream>
 #include "fft_lib.h"
+#include <iostream>
 
 const double kPI = 3.141592653589793238460;
 
+namespace taylortrack {
+  namespace utils {
+    void FftLib::fft(CArray &x) {
+      const size_t N = x.size();
+      if (N <= 1) return;
 
+      // divide
+      CArray even = x[std::slice(0, N / 2, 2)];
+      CArray odd = x[std::slice(1, N / 2, 2)];
 
-namespace taylortrack{
-    namespace utils{
-        void FftLib::fft(CArray &x) {
+      // conquer
+      fft(even);
+      fft(odd);
 
-            const size_t N = x.size();
-            if (N <= 1) return;
-
-            // divide
-            CArray even = x[std::slice(0, N/2, 2)];
-            CArray  odd = x[std::slice(1, N/2, 2)];
-
-            // conquer
-            fft(even);
-            fft(odd);
-
-            // combine
-            for (size_t k = 0; k < N/2; ++k) {
-                ComplexDouble t = std::polar(1.0, -2 * kPI * k / N) * odd[k];
-                x[k    ] = even[k] + t;
-                x[k+N/2] = even[k] - t;
-            }
-        }
-
-
-        void FftLib::ifft(CArray &x) {
-
-            // conjugate the complex numbers
-            x = x.apply(std::conj);
-
-            // forward fft
-            fft(x);
-
-            // conjugate the complex numbers again
-            x = x.apply(std::conj);
-
-            // scale the numbers
-            x /= x.size();
-        }
-
-
-
-        void FftLib::fftshift(RArray &outvec, RArray &invec) {
-            // xdim is 1 since we only deal with col vectors
-            // xshift is 0 since we obviously never shift vectors along the x or row axis
-            unsigned long yshift = (unsigned long) floor(invec.size()/2);
-            fft_strategy::circshift(outvec,invec,1,invec.size(),0, yshift);
-
-        }
-
-
+      // combine
+      for (size_t k = 0; k < N / 2; ++k) {
+        ComplexDouble t = std::polar(1.0, -2 * kPI * k / N) * odd[k];
+        x[k] = even[k] + t;
+        x[k + N / 2] = even[k] - t;
+      }
     }
-}
+
+    void FftLib::ifft(CArray &x) {
+      // conjugate the complex numbers
+      x = x.apply(std::conj);
+
+      // forward fft
+      fft(x);
+
+      // conjugate the complex numbers again
+      x = x.apply(std::conj);
+
+      // scale the numbers
+      x /= x.size();
+    }
+
+    void FftLib::fftshift(RArray &outvec, RArray &invec) {
+      // xdim is 1 since we only deal with col vectors
+      // xshift is 0 since we obviously never shift vectors along the x or row axis
+      unsigned long yshift = (unsigned long) floor(invec.size() / 2);
+      fft_strategy::circshift(outvec, invec, 1, invec.size(), 0, yshift);
+    }
+  } // namsespace utils
+} // namespace taylortrack
--- src/utils/fft_lib.h
+++ src/utils/fft_lib.h
@@ -7,43 +7,42 @@
 #ifndef TAYLORTRACK_FFT_LIB_H
 #define TAYLORTRACK_FFT_LIB_H
 
+#include "fft_strategy.h"
 #include <valarray>
 #include <complex>
-#include "fft_strategy.h"
 
 namespace taylortrack {
-    namespace utils {
-        /**
-        * @class fft_strategy
-        * @brief TODO
-        *
-        * TODO detailed description if necessary
-        */
-        class FftLib: public fft_strategy{
-        public:
-            typedef std::complex<double> ComplexDouble;
-            typedef std::valarray<double> RArray;
-            typedef std::valarray<ComplexDouble> CArray;
+  namespace utils {
+    /**
+    * @class fft_strategy
+    * @brief TODO
+    *
+    * TODO detailed description if necessary
+    */
+    class FftLib: public fft_strategy {
+     public:
+      typedef std::complex<double> ComplexDouble;
+      typedef std::valarray<double> RArray;
+      typedef std::valarray<ComplexDouble> CArray;
 
-            /**
-            * @brief Perform a fast fourier transformation on a signal
-            * @param x Discrete audio signal.
-            */
-            void fft(CArray &x);
-            /**
-            * @brief Perform an inverse fast fourier transformation on a signal
-            * @param x Discrete audio signal.
-            */
-            void ifft(CArray& x);
+      /**
+      * @brief Perform a fast fourier transformation on a signal
+      * @param x Discrete audio signal.
+      */
+      void fft(CArray &x);
+      /**
+      * @brief Perform an inverse fast fourier transformation on a signal
+      * @param x Discrete audio signal.
+      */
+      void ifft(CArray &x);
 
-            /**
-            * @brief Performs a fftshift on a given valarray and writes the shifted vector into another given valarray.
-            * @param outvec The valarray that has to contain the shifted valarray
-            * @param invec The valarray that contains the original valarray
-            */
-            void fftshift(RArray &outvec, RArray &invec);
-
-        };
-    } //namespace utils
+      /**
+      * @brief Performs a fftshift on a given valarray and writes the shifted vector into another given valarray.
+      * @param outvec The valarray that has to contain the shifted valarray
+      * @param invec The valarray that contains the original valarray
+      */
+      void fftshift(RArray &outvec, RArray &invec);
+    };
+  } //namespace utils
 } // namespace taylortrack
 #endif //TAYLORTRACK_FFT_LIB_H
