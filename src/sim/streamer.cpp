eaf052ab3187fa70933f8a3b630fde0a75c888a5
christian <christian.hack2@googlemail.com>
2016-02-14 20:10:13 +0100

    adjusted code to the google c++ style guide,

--- src/sim/streamer.cpp
+++ src/sim/streamer.cpp
@@ -6,31 +6,32 @@
 #include "streamer.h"
 
 namespace taylortrack {
-    namespace sim {
-        Streamer::Streamer(taylortrack::input::InputStrategy* strategy, const char* outport) {
-            strategy_ = strategy;
-            outport_ = outport;
-        }
+  namespace sim {
+    Streamer::Streamer(taylortrack::input::InputStrategy *strategy, const char *outport) {
+      strategy_ = strategy;
+      outport_ = outport;
+    }
 
-        bool Streamer::start_streaming(const char* inport) {
-            if(strategy_->is_done()) {
-                return false;
-            } else {
-                // Initialize YARP Network
-                yarp::os::Network yarp;
-                yarp::os::BufferedPort<yarp::os::Bottle> out_port;
-                out_port.open(outport_);
+    bool Streamer::start_streaming(const char *inport) {
+      if (strategy_->is_done()) {
+        return false;
+      } else {
+        // Initialize YARP Network
+        yarp::os::Network yarp;
+        yarp::os::BufferedPort<yarp::os::Bottle> out_port;
+        out_port.open(outport_);
 
-                yarp.connect(out_port.getName(),yarp::os::ConstString(inport));
+        yarp.connect(out_port.getName(), yarp::os::ConstString(inport));
 
-                while(!strategy_->is_done()) {
-                    yarp::os::Bottle& output = out_port.prepare();
-                    output.clear();
-                    strategy_->read(output);
-                    out_port.write(true); //blocking statement
-                } // while
-                return true;
-            } //if
-        }
-    } // namespace sim
+        while (!strategy_->is_done()) {
+          yarp::os::Bottle &output = out_port.prepare();
+          output.clear();
+          strategy_->read(output);
+          out_port.write(true); //blocking statement
+        } // while
+        return true;
+      } // if
+    }
+
+  } // namespace sim
 } // namespace taylortrack
--- src/sim/streamer.h
+++ src/sim/streamer.h
@@ -8,36 +8,36 @@
 #include <stdbool.h>
 #include "../input/input_strategy.h"
 
- namespace taylortrack {
-    namespace sim {
-        /**
-         * @class Streamer
-         * @brief Starts streaming with specific strategy
-         */
-        class Streamer {
-        private:
+namespace taylortrack {
+  namespace sim {
+    /**
+     * @class Streamer
+     * @brief Starts streaming with specific strategy
+     */
+    class Streamer {
+     public:
+      /**
+       * @brief Streamer Constructor
+       *
+       * @param strategy defines which strategy to use for streaming
+       * @param outport write port (for connecting with readport)
+       */
+      Streamer(taylortrack::input::InputStrategy *strategy, const char *outport);
 
-        public:
-            /**
-             * @brief Streamer Constructor
-             * 
-             * @param strategy defines which strategy to use for streaming
-             * @param outport write port (for connecting with readport)
-             */
-            Streamer(taylortrack::input::InputStrategy* strategy, const char* outport);
-            /**
-             * @brief starts streaming
-             * 
-             * @param inport to retrieve all streamed data
-             * @return true if streaming is done
-             * @see start_streaming()
-             */
-            bool start_streaming(const char* inport);
-        private:
-            taylortrack::input::InputStrategy* strategy_;
-            const char *outport_;
-        };
-    } // namespace sim
- } // namespace taylortrack
- #endif // TAYLORTRACK_SIM_STREAMER_H_
+      /**
+       * @brief starts streaming
+       *
+       * @param inport to retrieve all streamed data
+       * @return true if streaming is done
+       * @see start_streaming()
+       */
+      bool start_streaming(const char *inport);
+
+     private:
+      taylortrack::input::InputStrategy *strategy_;
+      const char *outport_;
+    };
+  } // namespace sim
+} // namespace taylortrack
+#endif // TAYLORTRACK_SIM_STREAMER_H_
 
