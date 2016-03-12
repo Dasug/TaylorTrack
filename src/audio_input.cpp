0221190949c22501c3a9880620e0598835edf7c0
Marius Kaufmann <marius.kaufmann@googlemail.com>
2016-03-12 13:09:38 +0100

    added cmake target and created c++ file for audio input

---
+++ src/audio_input.cpp
@@ -0,0 +1,28 @@
+/**
+ * @file
+ * @brief Simulation for streaming data
+ */
+
+
+#include "input/input_strategy.h"
+#include "sim/streamer.h"
+#include "utils/parameter_parser.h"
+#include "input/wave_input_strategy.h"
+
+/**
+ * @brief streaming data main method
+ *
+ * Reads a file with the Read_File_Input strategy and starts streaming the file
+ */
+int main(int argc, const char *argv[]) {
+    /*taylortrack::utils::Parameters parameters = taylortrack::utils::ParameterParser::parse_streamer(argc,argv);
+    if(!parameters.valid) {
+        std::cout << "Invalid parameter usage. Please check --help" << std::endl;
+        return EXIT_FAILURE;
+    } else {
+        taylortrack::input::WaveInputStrategy dummy = taylortrack::input::WaveInputStrategy(parameters);
+        taylortrack::sim::Streamer streamer = taylortrack::sim::Streamer(&dummy, parameters.outport);
+        streamer.start_streaming(parameters.inport);
+        return EXIT_SUCCESS;
+    }*/
+}
