11de895948e259039093a7f33eec3da86c6007dc
Marius Kaufmann <marius.kaufmann@googlemail.com>
2016-03-15 14:46:55 +0100

    data streamer is now build dynamically with the selected input strategy using compile flags

--- src/sim_datastreamer.cpp
+++ src/sim_datastreamer.cpp
@@ -22,8 +22,16 @@ int main(int argc, const char *argv[]) {
         std::cout << "Invalid parameter usage. Please check --help" << std::endl;
         return EXIT_FAILURE;
     } else {
-        taylortrack::input::WaveInputStrategy dummy = taylortrack::input::WaveInputStrategy(parameters);
-        taylortrack::sim::Streamer streamer = taylortrack::sim::Streamer(&dummy, parameters.outport);
+#if defined INPUT_DUMMY
+        taylortrack::input::DummyInputStrategy strategy = taylortrack::input::DummyInputStrategy();
+#elif defined INPUT_READ_FILE
+        taylortrack::input::ReadFileInputStrategy strategy = taylortrack::input::ReadFileInputStrategy(parameters);
+#elif defined INPUT_WAVE_FILE
+        taylortrack::input::WaveInputStrategy strategy = taylortrack::input::WaveInputStrategy(parameters);
+#else
+#error Missing or unknown input strategy
+#endif
+        taylortrack::sim::Streamer streamer = taylortrack::sim::Streamer(&strategy, parameters.outport);
         streamer.start_streaming(parameters.inport);
         return EXIT_SUCCESS;
     }
