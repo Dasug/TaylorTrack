e5dfd15318267ed3fc6ec3990e4f1d70009b9413
jannis hoppe <jannis.hoppe89@gmail.com>
2016-03-24 16:33:32 +0100

    parameter parser has been linted

--- src/utils/parameter_parser.cpp
+++ src/utils/parameter_parser.cpp
@@ -1,28 +1,53 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2015 Marius Kaufmann, Tamara Frieß, Jannis Hoppe, Christian Hack
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 /**
  * @file
  * @brief Parser
  */
-#include "parameter_parser.h"
+#include "utils/parameter_parser.h"
 #include <iostream>
 
-taylortrack::utils::Parameters taylortrack::utils::ParameterParser::parse_streamer(int argc,const char **argv) {
+taylortrack::utils::Parameters
+taylortrack::utils::ParameterParser::parse_streamer(int argc,
+                                                    const char **argv) {
   char *end;
   Parameters parameters = utils::Parameters();
 
-  for(int i = 1; i < argc; i++) {
-    if(argv[i][0] == '-') {
-      switch(argv[i][1]) {
+  for (int i = 1; i < argc; i++) {
+    if (argv[i][0] == '-') {
+      switch (argv[i][1]) {
         // stream buffer size
         case 's':
-          if(++i >= argc) {
+          if (++i >= argc) {
             parameters.valid = false;
           } else {
-            parameters.size =  (int) strtol(argv[i], &end, 10);
+            parameters.size =  static_cast<int>(strtol(argv[i], &end, 10));
           }
           break;
         // Output port
         case 'o':
-          if(++i >= argc) {
+          if (++i >= argc) {
             parameters.valid = false;
           } else {
             parameters.outport = argv[i];
@@ -30,7 +55,7 @@ taylortrack::utils::Parameters taylortrack::utils::ParameterParser::parse_stream
           break;
         // Input port
         case 'i':
-          if(++i >= argc) {
+          if (++i >= argc) {
             parameters.valid = false;
           } else {
             parameters.inport = argv[i];
@@ -40,10 +65,10 @@ taylortrack::utils::Parameters taylortrack::utils::ParameterParser::parse_stream
         default:
           parameters.valid = false;
       }
-    }else {
+    } else {
       parameters.file = argv[i];
       parameters.valid = true;
     }
   }
   return parameters;
-} // LCOV_EXCL_BR_LINE
+}  // LCOV_EXCL_BR_LINE
--- src/utils/parameter_parser.h
+++ src/utils/parameter_parser.h
@@ -1,3 +1,26 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2015 Marius Kaufmann, Tamara Frieß, Jannis Hoppe, Christian Hack
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 /**
  * @file
  * @brief Provides a parser for the start parameters.
@@ -5,25 +28,26 @@
  */
 #ifndef TAYLORTRACK_PARAMETER_PARSER_H
 #define TAYLORTRACK_PARAMETER_PARSER_H
-#include "parameters.h"
+#include "utils/parameters.h"
 namespace taylortrack {
-  namespace utils {
+namespace utils {
+/**
+* @class ParameterParser
+* @brief Provides parser for the start parameters.
+*/
+class ParameterParser {
+ public:
   /**
-   * @class ParameterParser
-   * @brief Provides parser for the start parameters.
+   * @brief Parser for the streaming module.
+   *
+   * @param argc Amount of passed parameters
+   * @param argv Passed Parameters.
+   * @return Returns file path, the size in which the packets shall be streamed, the port name and a valid flag.
    */
-  class ParameterParser {
-    public:
-      /**
-       * @brief Parser for the streaming module.
-       *
-       * @param argc Amount of passed parameters
-       * @param argv Passed Parameters.
-       * @return Returns file path, the size in which the packets shall be streamed, the port name and a valid flag.
-       */
-      static Parameters parse_streamer(int argc, const char **argv);
-    };
-  } // namespace utils
-} // namespace taylortrack
+  static Parameters parse_streamer(int argc, const char **argv);
+};
+}  // namespace utils
+}  // namespace taylortrack
 
-#endif //TAYLORTRACK_PARAMETER_PARSER_H
+#endif  // TAYLORTRACK_PARAMETER_PARSER_H
+
