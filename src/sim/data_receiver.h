975f0f4e919b0381161d5e52ffacd6d0437c38e5
Marius Kaufmann <marius.kaufmann@googlemail.com>
2016-03-28 15:01:31 +0200

    unindented namespace in data_receiver

--- src/sim/data_receiver.h
+++ src/sim/data_receiver.h
@@ -5,19 +5,19 @@
 #include "utils/config.h"
 
 namespace taylortrack {
-  namespace sim {
-    class DataReceiver {
-     public:
-      DataReceiver(const taylortrack::utils::CommunicationSettings &in_settings);
-      ~DataReceiver();
-      std::vector<double> readData(bool blocking = true) const;
+namespace sim {
+class DataReceiver {
+ public:
+  DataReceiver(const taylortrack::utils::CommunicationSettings &in_settings);
+  ~DataReceiver();
+  std::vector<double> readData(bool blocking = true) const;
 
-     private:
-      taylortrack::utils::CommunicationSettings in_settings_;
-      yarp::os::Network yarp_;
-      yarp::os::BufferedPort<yarp::os::Bottle> *buffered_port_;
-    };
-  } // namespace sim
+ private:
+  taylortrack::utils::CommunicationSettings in_settings_;
+  yarp::os::Network yarp_;
+  yarp::os::BufferedPort<yarp::os::Bottle> *buffered_port_;
+};
+} // namespace sim
 } // namespace taylortrack
 
 #endif //TAYLORTRACK_DATA_RECEIVER_H_
